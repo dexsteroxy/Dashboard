17d048b9cd3457a872341ddc096fbd57e2cbc89b
Austin Amadi <amadiaustin@microsoft.com>
2023-11-08 17:38:35 +0100

    adding a pie chart

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,33 +1,46 @@
 import React from 'react';
-import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Doughnut = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
-    <SparklineComponent
-    id={id}
-    height={height}
-    width={width}
-    lineWidth={1}
-    valueType="Numeric"
-    fill={color}
-    border={{ color: currentColor, width: 2 }}
-    tooltipSettings={{
-      visible: true,
-      // eslint-disable-next-line no-template-curly-in-string
-      format: '${x} : data ${yval}',
-      trackLineSettings: {
-        visible: true,
-      },
-    }}
-    markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
-    dataSource={data}
-    xName="x"
-    yName="yval"
-    type={type}
-  >
-    <Inject services={[SparklineTooltip]} />
-  </SparklineComponent>
-  )
-}
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{ visible: legendVisiblity, background: 'white' }}
+      height={height}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      tooltip={{ enable: true }}
+    >
+      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: 'text',
+            position: 'Inside',
+            font: {
+              fontWeight: '600',
+              color: '#fff',
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
+};
 
-export default SparkLine
+export default Doughnut;
